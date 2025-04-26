08e5131f4f99fd55f75219b17cbd1990073aa6f8
AlfredoMate <mate.alfredo98@gmail.com>
2025-04-26 17:41:02 +0200

    spotless fix

--- src/main/java/io/autoinvestor/configuration/SecurityConfiguration.java
+++ src/main/java/io/autoinvestor/configuration/SecurityConfiguration.java
@@ -1,29 +1,19 @@
 package io.autoinvestor.configuration;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.web.server.ServerHttpSecurity;
-import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
-import org.springframework.security.oauth2.jwt.NimbusReactiveJwtDecoder;
-import org.springframework.security.oauth2.jwt.ReactiveJwtDecoder;
-import org.springframework.security.web.server.SecurityWebFilterChain;
-
-//@Configuration
+// @Configuration
 public class SecurityConfiguration {
 
-    //@Bean
-    //public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
-    //    http.csrf(ServerHttpSecurity.CsrfSpec::disable).authorizeExchange().anyExchange().permitAll();
-    //    return http.build();
-    //}
-/*
-    @Bean
-    public ReactiveJwtDecoder reactiveJwtDecoder() {
-        return NimbusReactiveJwtDecoder
-                .withJwkSetUri("https://dev-90706258.okta.com/oauth2/v1/keys")
-                .build();
-    }
-    */
-
+    // @Bean
+    // public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http)
+    // {
+    //
+    // http.csrf(ServerHttpSecurity.CsrfSpec::disable).authorizeExchange().anyExchange().permitAll();
+    // return http.build();
+    // }
+    /*
+     * @Bean public ReactiveJwtDecoder reactiveJwtDecoder() { return
+     * NimbusReactiveJwtDecoder
+     * .withJwkSetUri("https://dev-90706258.okta.com/oauth2/v1/keys") .build(); }
+     */
 
 }
