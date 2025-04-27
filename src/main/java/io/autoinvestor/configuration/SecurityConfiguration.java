1a86acbe3a061e62711107c5de2ae4f71c809f4a
AlfredoMate <mate.alfredo98@gmail.com>
2025-04-27 11:18:14 +0200

    web security config

--- src/main/java/io/autoinvestor/configuration/SecurityConfiguration.java
+++ src/main/java/io/autoinvestor/configuration/SecurityConfiguration.java
@@ -1,19 +1,14 @@
 package io.autoinvestor.configuration;
 
-// @Configuration
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
 public class SecurityConfiguration {
 
-    // @Bean
-    // public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http)
-    // {
-    //
-    // http.csrf(ServerHttpSecurity.CsrfSpec::disable).authorizeExchange().anyExchange().permitAll();
-    // return http.build();
-    // }
-    /*
-     * @Bean public ReactiveJwtDecoder reactiveJwtDecoder() { return
-     * NimbusReactiveJwtDecoder
-     * .withJwkSetUri("https://dev-90706258.okta.com/oauth2/v1/keys") .build(); }
-     */
-
+    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
+        http.csrf(ServerHttpSecurity.CsrfSpec::disable).authorizeExchange().anyExchange().permitAll();
+        return http.build();
+    }
 }
