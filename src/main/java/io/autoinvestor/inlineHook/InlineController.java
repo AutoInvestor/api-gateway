79e69c7d9374c29287dff2c376b27f60a91788f7
alvaromaoc <alvaromaoc@gmail.com>
2025-05-12 21:27:15 +0200

    Login

--- src/main/java/io/autoinvestor/inlineHook/InlineController.java
+++ src/main/java/io/autoinvestor/inlineHook/InlineController.java
@@ -7,6 +7,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
 
 import java.util.List;
